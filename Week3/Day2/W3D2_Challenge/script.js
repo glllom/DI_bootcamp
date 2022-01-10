585ff44f43407a77e34be32460b43c9b8401d307
Gleb Omarov <gleb.omarov@gmail.com>
2022-01-10 20:46:27 +0200

    new files

---
+++ Week3/Day2/W3D2_Challenge/script.js
@@ -0,0 +1,19 @@
+/*
+ * Exercice 1
+ */
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+fruits.sort();
+fruits.push("Kiwi");
+fruits.splice(0,1);
+fruits.reverse();
+console.log(fruits);
+
+/*
+ * Exercice 2
+ */
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0]);
