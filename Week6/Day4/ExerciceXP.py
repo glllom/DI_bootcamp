f7a85a346d0e2a521b46bd94df3712f8f8578b0d
Gleb Omarov <gleb.omarov@gmail.com>
2022-02-03 18:08:27 +0200

    Week 6 Day 4

--- Week6/Day4/ExerciceXP.py
+++ Week6/Day4/ExerciceXP.py
@@ -1,5 +1,10 @@
 """
 Exercise 1 : Favorite Numbers
+Create a set called my_fav_numbers with all your favorites numbers.
+Add two new numbers to the set.
+Remove the last number.
+Create a set called friend_fav_numbers with your friend’s favorites numbers.
+Concatenate my_fav_numbers and friend_fav_numbers to a new variable called our_fav_numbers.
 """
 my_fav_numbers = set()
 my_fav_numbers.add(21)
@@ -15,8 +20,11 @@ print(our_fav_numbers)
 """
 Exercise 2: Tuple
 Given a tuple which value is integers, is it possible to add more integers to the tuple?
-Yes it possible
+Yes it possible by another tuple
 """
+my_tuple = (1, 3)
+my_tuple += (5, 6)
+print(my_tuple)
 
 """
 Exercise 3: Print A Range Of Numbers
