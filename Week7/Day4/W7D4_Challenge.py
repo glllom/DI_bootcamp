8177a5ff0c1d95223ae0e2a3517cb7fa69b74541
Gleb Omarov <gleb.omarov@gmail.com>
2022-02-13 14:54:13 +0200

    Week 7 Day 4

--- Week7/Day4/W7D4_Challenge.py
+++ Week7/Day4/W7D4_Challenge.py
@@ -1,8 +1,8 @@
 def division(a, b):
     try:
         return a / b
-    except ZeroDivisionError:
-        print("You can not divide by zero!")
+    except ZeroDivisionError as e:
+        print("You can not divide by zero! Error -- ", e)
 
 
 print(division(5, 0))
