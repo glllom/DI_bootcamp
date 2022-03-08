ee7d6ace3823abe209a924cafb91f8244ba1d6d0
gleb <gleb.omarov@gmail.com>
2022-03-08 17:27:30 +0200

    Week 10 day 5

---
+++ Week11/Day2/app.py
@@ -0,0 +1,15 @@
+import flask
+"""Exercise 1"""
+print(dir(flask))
+
+"""Exercise2"""
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def cv():
+    return flask.render_template('cv.html', name="Gleb Omarov")
+
+
+if __name__ == '__main__':
+    app.run()
