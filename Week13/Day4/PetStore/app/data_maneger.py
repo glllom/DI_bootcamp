2ee2f0898e57511a8dc5fc567e62dbd00172412f
gleb <gleb.omarov@gmail.com>
2022-03-24 14:12:41 +0200

    Week 13 day 4

--- Week13/Day4/PetStore/app/data_maneger.py
+++ Week13/Day4/PetStore/app/data_maneger.py
@@ -1,1 +1,64 @@
 from app.models import Pet
+from app import db
+import requests
+#  2
+new_pet = Pet(name='Jaw',
+              gender='M',
+              breed='dingo',
+              details='A wild one!',
+              price=5,
+              image=(requests.get("https://dog.ceo/api/breed/dingo/images/random")).json()['message']
+              )
+db.session.add(new_pet)
+
+
+#  3
+new_pet = Pet(name='Sweety',
+              gender='F',
+              breed='chihuahua',
+              details='Small and pretty.',
+              price=700,
+              image=(requests.get("https://dog.ceo/api/breed/chihuahua/images/random")).json()['message']
+              )
+db.session.add(new_pet)
+
+
+#  4
+new_pet = Pet(name='Nanny',
+              gender='F',
+              breed='golden retriever',
+              details='Where is a baby? I am the best nanny!',
+              price=300,
+              image=(requests.get("https://dog.ceo/api/breed/retriever/golden/images/random")).json()['message']
+              )
+db.session.add(new_pet)
+
+
+#  5
+new_pet = Pet(name='Pathfinder',
+              gender='M',
+              breed='border collie',
+              details='Sheep are so stupid.',
+              price=1000,
+              image=(requests.get("https://dog.ceo/api/breed/collie/border/images/random")).json()['message']
+              )
+db.session.add(new_pet)
+
+
+#  6
+new_pet = Pet(name='mr.Captain',
+              gender='M',
+              breed='doberman',
+              details='I serve the people!',
+              price=515,
+              image=(requests.get("https://dog.ceo/api/breed/doberman/images/random")).json()['message']
+              )
+db.session.add(new_pet)
+
+
+
+
+
+
+
+db.session.commit()
--- Week13/Day4/PetStore/app/routes.py
+++ Week13/Day4/PetStore/app/routes.py
@@ -1,5 +1,6 @@
 from app import flask_app
 from flask import render_template, redirect, url_for
+from app.models import Pet
 
 
 @flask_app.route("/")
@@ -9,9 +10,11 @@ def index():
 
 @flask_app.route("/pets")
 def all_pets():
-    return render_template('pets.html')
+    pets = Pet.query.all()
+    return render_template('pets.html', pets=pets)
 
 
 @flask_app.route("/pet/<int:pet_id>")
 def pet_info(pet_id):
-    return render_template('pet.html')
+    pet = Pet.query.get(pet_id)
+    return render_template('pet.html', pet=pet)
